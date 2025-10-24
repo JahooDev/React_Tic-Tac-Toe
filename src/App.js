003b01d96038fdc0e3850c1fefdb06944877ee11
Jan <jahoo1990@gmail.com>
2025-10-24 14:25:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+ function Square() {
+  return <button className="square" onClick={console.log("button clicked")}>X</button>;
+}
+
+
+
+export default function Board() {
+  return (
+  <>
+    <div>
+      <div className="status">Status here</div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  </>
+  )
+}
